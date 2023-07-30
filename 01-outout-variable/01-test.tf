1300632a782509584be6141c8737612af875b6cb
Senthil Natesan <senthil.natesan@atos.net>
2023-07-30 21:56:11 +0530

    output - varible with List added and  used in string

--- 01-outout-variable/01-test.tf
+++ 01-outout-variable/01-test.tf
@@ -10,3 +10,13 @@ output "sample-var-out" {
   value = "Welcomr to ${var.sample}"
  
 }
+
+
+variable "examble-list" {
+  default = ["Hello",100,"world",10]
+}
+
+output "examble-list-out" {
+  value = "${var.examble-list[0]} to ${var.examble-list[2]} start ${var.examble-list-out[3]}"
+  
+}
